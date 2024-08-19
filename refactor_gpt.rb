5341cbbf439cb0e58929a11aed05f35817ebe722
Peter M Adrianov <peter.adrianov@gmail.com>
2024-08-19 20:44:43 +0300

    Refactor.

--- refactor_gpt.rb
+++ refactor_gpt.rb
@@ -1,52 +1,46 @@
 #!/usr/bin/env ruby
 require 'net/http'
-require 'json'
+require 'oj'
 require 'retries'
 
 # Class to interact with OpenAI API
 class OpenAi
   def initialize
-    @api_base_url = fetch_env('OPENAI_BASE_URL')
-    @api_key = fetch_env('OPENAI_ACCESS_TOKEN')
+    @api_base_url = fetch_env_variable('OPENAI_BASE_URL')
+    @api_key = fetch_env_variable('OPENAI_ACCESS_TOKEN')
     @model = 'gpt-4o'
     @temperature = 0
   end
 
   # Method to send prompts to OpenAI API and get a response
   def ask(prompts)
-    response_body = ''
-    answer = with_retries(base_sleep_seconds: 5, max_sleep_seconds: 5, rescue: [Net::ReadTimeout]) do
+    with_retries(base_sleep_seconds: 5, max_sleep_seconds: 5, rescue: [Net::ReadTimeout]) do
       uri = URI(@api_base_url + '/chat/completions')
       request = Net::HTTP::Post.new(uri, 'Content-Type' => 'application/json', 'Authorization' => "Bearer #{@api_key}")
-      request.body = { model: @model, temperature: @temperature, messages: prompts, response_format: { type: 'json_object' } }.to_json
+      request.body = Oj.dump({ model: @model, temperature: @temperature, messages: prompts, response_format: { type: 'json_object' } }, mode: :compat, symbol_keys: true)
       response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https', read_timeout: 100) { |http| http.request(request) }
-      response_body = response.body.to_s
-      JSON.parse(response_body).dig('choices', 0, 'message', 'content') rescue {}
+      parse_response(response)
     end
-
-    if answer.nil? || answer.empty?
-      puts response_body
-      exit
-    end
-
-    extract_json(answer)
   end
 
-  # Method to extract JSON from the response
-  def extract_json(answer)
-    (answer.match(/{.*}/m) || answer.match(/\[.*\]/m) || answer.match(/".*"|\d+(\.\d+)?|true|false|null/)).to_s
+  # Method to parse the response and handle errors
+  def parse_response(response)
+    response_body = response&.body.to_s
+    answer = Oj.load(response_body).dig('choices', 0, 'message', 'content') rescue {}
+    return answer unless answer.nil? || answer.empty?
+
+    puts response_body
+    exit
   end
 
   # Method to refactor code using OpenAI
   def refactor(code, additional_instructions = nil)
     response = ask([{
       role: 'system',
-      content: <<~TEXT
-        Refactor software code. Return complete code block only in JSON {"code": "..."}.
-      TEXT
+      content: "Refactor software code. Return complete code block only in JSON {\"code\": \"...\"}."
     }, {
       role: 'user',
-      content: <<~TEXT
+      content: <<~HEREDOC
         Refactor the following software code according to these guidelines:
 
         1. Error Handling: Identify and fix any errors by rewriting the affected sections if necessary.
@@ -56,29 +50,28 @@ class OpenAi
         5. Simplify Logic: Reduce the number of assignments, branches, and conditions.
         6. Comments: Add a brief comment before each class or function to explain its purpose.
         7. Preserve Logic: Maintain all existing business logic.
-        
+
         Return the complete refactored code block only without explanations.
-        
+
         #{additional_instructions ? 'Additional user instructions: ' + additional_instructions + '.' : ''}
-        
+
         ```
         #{code}
         ```
-      TEXT
+      HEREDOC
     }])
-    JSON.parse(response)['code']
+    Oj.load(response)['code']
   end
 
   private
 
   # Method to fetch environment variables from .env file
-  def fetch_env(key, default = nil)
+  def fetch_env_variable(key, default = nil)
     env_file = File.join(File.dirname(__FILE__), '.env')
     return default unless File.exist?(env_file)
 
     File.foreach(env_file) do |line|
-      next unless line.start_with?(key)
-      return line.split('=')[1].strip
+      return line.split('=')[1].strip if line.start_with?(key)
     end
     default
   end
