bf11360789f9f4115492688056fb336a9fdb8677
Peter M Adrianov <peter.adrianov@gmail.com>
2026-02-01 15:18:41 +0300

    test: add tests for request preparer model hint extraction

---
+++ test/request_preparer_test.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require_relative '../lib/loader'
+
+class TestRequestPreparer < Minitest::Test
+  def models
+    Superagent::MODELS
+  end
+
+  def test_extract_model_index_exact_tag
+    assert_equal 0, RequestPreparer.extract_model_index('@auto fix bug', models)
+    assert_equal 1, RequestPreparer.extract_model_index('use @grok', models)
+  end
+
+  def test_extract_model_index_word_boundary_tag
+    assert_equal 4, RequestPreparer.extract_model_index('@sonnet refactor', models)
+    assert_equal 5, RequestPreparer.extract_model_index('@opus please', models)
+    assert_equal 3, RequestPreparer.extract_model_index('@composer help', models)
+    assert_equal 2, RequestPreparer.extract_model_index('@gemini task', models)
+  end
+
+  def test_extract_model_index_unknown_tag_returns_nil
+    assert_nil RequestPreparer.extract_model_index('@unknown task', models)
+  end
+
+  def test_sanitize_request_strips_model_mentions
+    assert_equal 'fix bug', RequestPreparer.sanitize_request('@sonnet fix bug', models)
+    assert_equal 'refactor', RequestPreparer.sanitize_request('refactor @opus', models)
+  end
+
+  def test_sanitize_request_keeps_unknown_mentions
+    assert_equal 'ask @unknown', RequestPreparer.sanitize_request('ask @unknown', models)
+  end
+end
