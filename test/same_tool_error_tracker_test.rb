696a3c12948eccc7f69f89b65ccc4ff276eaa99f
Peter M Adrianov <peter.adrianov@gmail.com>
2026-02-01 15:58:22 +0300

    test: add tests for same tool error tracker threshold behavior

---
+++ test/same_tool_error_tracker_test.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require_relative '../lib/loader'
+
+class TestSameToolErrorTracker < Minitest::Test
+  def test_three_errors_same_key_returns_interrupt_on_third
+    tracker = SameToolErrorTracker.new
+    assert_equal :ok, tracker.record('run_command', error: true)
+    assert_equal :ok, tracker.record('run_command', error: true)
+    assert_equal :interrupt, tracker.record('run_command', error: true)
+  end
+
+  def test_success_resets_count_for_key
+    tracker = SameToolErrorTracker.new
+    tracker.record('run_command', error: true)
+    tracker.record('run_command', error: true)
+    tracker.record('run_command', error: false)
+    assert_equal :ok, tracker.record('run_command', error: true)
+    assert_equal :ok, tracker.record('run_command', error: true)
+    assert_equal :interrupt, tracker.record('run_command', error: true)
+  end
+
+  def test_different_keys_tracked_independently
+    tracker = SameToolErrorTracker.new
+    tracker.record('key_a', error: true)
+    tracker.record('key_a', error: true)
+    assert_equal :ok, tracker.record('key_b', error: true)
+    assert_equal :interrupt, tracker.record('key_a', error: true)
+    assert_equal :ok, tracker.record('key_b', error: true)
+    assert_equal :interrupt, tracker.record('key_b', error: true)
+  end
+
+  def test_reset_clears_counts
+    tracker = SameToolErrorTracker.new
+    tracker.record('run_command', error: true)
+    tracker.record('run_command', error: true)
+    tracker.reset
+    assert_equal :ok, tracker.record('run_command', error: true)
+    assert_equal :ok, tracker.record('run_command', error: true)
+    assert_equal :interrupt, tracker.record('run_command', error: true)
+  end
+
+  def test_nil_key_returns_ok
+    tracker = SameToolErrorTracker.new
+    assert_equal :ok, tracker.record(nil, error: true)
+    assert_equal :ok, tracker.record(nil, error: true)
+  end
+
+  def test_custom_threshold
+    tracker = SameToolErrorTracker.new(threshold: 2)
+    assert_equal :ok, tracker.record('key', error: true)
+    assert_equal :interrupt, tracker.record('key', error: true)
+  end
+end
