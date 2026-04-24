d4b202b9e526122950f6a281d606007bbc8ab682
Peter M Adrianov <peter.adrianov@gmail.com>
2026-04-24 13:11:21 +0300

    chore: update default model from glm-5 to glm-5.1

--- lib/openai_client.rb
+++ lib/openai_client.rb
@@ -37,7 +37,7 @@ class OpenAiClient
   include PrimaryApiBackoff
   include PrimaryApiErrorBody
   attr_reader :model
-  DEFAULT_MODEL = "glm-5"
+DEFAULT_MODEL = "glm-5.1"
   REQUEST_TIMEOUT = 300
   DEFAULT_PROGRESS_SPEED = 300
   PROGRESS_SPEED_FILE = File.join(Dir.home, ".refactor_gpt").freeze
