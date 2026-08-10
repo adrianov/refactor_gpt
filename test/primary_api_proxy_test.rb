77a47eaaa4be95afea8f02e759ee05b160cb17c5
Peter M Adrianov <peter.adrianov@gmail.com>
2026-08-11 01:07:29 +0300

    test: add PrimaryApiProxy normalization and env-var selection coverage

---
+++ test/primary_api_proxy_test.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require_relative '../lib/loader'
+
+class TestPrimaryApiProxy < Minitest::Test
+  def test_normalize_socks_schemes
+    assert_equal 'socks5://127.0.0.1:1080', PrimaryApiProxy.normalize('socks://127.0.0.1:1080')
+    assert_equal 'socks5://127.0.0.1:1080', PrimaryApiProxy.normalize('socks5h://127.0.0.1:1080')
+    assert_equal 'http://proxy:8080', PrimaryApiProxy.normalize('http://proxy:8080')
+  end
+
+  def test_choose_prefers_socks5_over_http_proxy
+    env = {
+      'HTTPS_PROXY' => 'http://proxy:8080',
+      'ALL_PROXY' => 'socks5://127.0.0.1:1080'
+    }
+    assert_equal 'socks5://127.0.0.1:1080', PrimaryApiProxy.resolve(nil, env)
+  end
+
+  def test_uses_https_proxy_when_no_socks
+    env = {'HTTPS_PROXY' => 'http://proxy:8080'}
+    assert_equal 'http://proxy:8080', PrimaryApiProxy.resolve(nil, env)
+  end
+
+  def test_explicit_proxy_url_included_and_socks_still_preferred
+    env = {'HTTPS_PROXY' => 'http://proxy:8080', 'ALL_PROXY' => 'socks://127.0.0.1:1080'}
+    assert_equal 'socks5://127.0.0.1:1080', PrimaryApiProxy.resolve('http://explicit:9', env)
+  end
+
+  def test_returns_nil_when_no_proxy_configured
+    assert_nil PrimaryApiProxy.resolve(nil, {})
+  end
+end
