db43c26cb0c31c7b54701309e9db9c7b1297b884
Peter M Adrianov <peter.adrianov@gmail.com>
2026-08-20 17:55:10 +0300

    feat: make loader re-exec project Ruby and drop foreign Bundler environment

--- lib/loader.rb
+++ lib/loader.rb
@@ -5,8 +5,21 @@
 # Shared types (SystemInfo, Utility, OpenAiClient, GeminiClient, etc.)
 # live in lib; AskGptClient/AskGeminiClient are used by ask_gpt only.
 # openai_client.rb defines OpenAiClient (capital I); Zeitwerk infers OpenaiClient from the path.
-require 'zeitwerk'
 REFACTOR_GPT_ROOT = File.expand_path('..', __dir__).freeze
+# Aliases follow cwd Ruby and Bundler; this process must use the project's interpreter and gems.
+wanted_ruby = File.read(File.join(REFACTOR_GPT_ROOT, '.ruby-version')).strip
+rbenv_ruby = File.expand_path("~/.rbenv/versions/#{wanted_ruby}/bin/ruby")
+foreign_bundle = ENV['BUNDLE_GEMFILE'] &&
+  File.expand_path(ENV['BUNDLE_GEMFILE']) != File.join(REFACTOR_GPT_ROOT, 'Gemfile')
+if (RUBY_VERSION != wanted_ruby || foreign_bundle) && File.executable?(rbenv_ruby) && File.file?($PROGRAM_NAME)
+  ENV.delete_if { |key, _| key.start_with?('BUNDLE_', 'BUNDLER_') }
+  rubyopt = ENV.delete('RUBYOPT').to_s.gsub(/(?:^|\s)-r\s*bundler\/setup/, '').strip
+  ENV['RUBYOPT'] = rubyopt unless rubyopt.empty?
+  ENV.delete('GEM_HOME')
+  ENV.delete('GEM_PATH')
+  exec(rbenv_ruby, File.expand_path($PROGRAM_NAME), *ARGV)
+end
+require 'zeitwerk'
 loader = Zeitwerk::Loader.new
 loader.push_dir(File.expand_path(__dir__))
 loader.collapse(File.expand_path('superagent', __dir__))
